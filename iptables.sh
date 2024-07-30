5fae0686888e3b23ea2fe0408577a4f074bc5acb
Pablo Cardozo <85005529+pjcdz@users.noreply.github.com>
2024-07-30 12:46:32 -0300

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
-# Permitir el tráfico entrante en el puerto 8080 solo desde la IP especificada
+# Limpiar reglas de iptables existentes
+iptables -F
+iptables -X
+
+# Establecer política por defecto para denegar todo el tráfico entrante
+iptables -P INPUT DROP
+
+# Permitir tráfico entrante en el puerto 8080 solo desde la IP especificada
 iptables -A INPUT -p tcp --dport 8080 -s 190.194.229.17 -j ACCEPT
-# Denegar todo el tráfico entrante en el puerto 8080 desde otras IPs
-iptables -A INPUT -p tcp --dport 8080 -j DROP
+
+# Permitir tráfico entrante en los puertos 11470 y 12470 para todas las IPs
+iptables -A INPUT -p tcp --dport 11470 -j ACCEPT
+iptables -A INPUT -p tcp --dport 12470 -j ACCEPT
+
+# Permitir tráfico entrante relacionado y establecido
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Permitir tráfico local (necesario para funcionamiento interno del contenedor)
+iptables -A INPUT -i lo -j ACCEPT
 
 # Ejecutar el script original de inicio del servicio
 exec "$@"
